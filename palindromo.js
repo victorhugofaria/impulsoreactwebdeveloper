cb8fdced4b051b119b6967d65f3427fffc50dc45
Victor Hugo <victorcobain@hotmail.com>
2021-11-09 16:14:10 -0400

    finalizandomodulodevariaveisetipos

---
+++ palindromo.js
@@ -0,0 +1,24 @@
+//solucao 1
+
+function verificaPalindromo(string){
+    if(!string) return "string inexistente";
+
+    return string.split("").reverse().join("") === string;
+}
+
+
+
+//solução 2
+
+
+function verificaPalindromo(string){
+    if(!string) return "string inexistente";
+
+    for(let i =0; i < string.length / 2; i++){
+        if(string[i] !== string[string.length -1 - i]) {
+        return false;
+        }
+    }
+    return true;
+}
+console.log(verificaPalindromo("abba"))
