c07c7dd22f581fcdc4b41b0ce5762c47d5972313
Victor Hugo <victorcobain@hotmail.com>
2021-11-17 13:45:59 -0400

    erros.js

---
+++ modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Victor", 34));
+console.log(mostraCidade('Victor', "Campo Grande"))
+console.log(mostraHobby("Victor", "Jogar video-game"))
