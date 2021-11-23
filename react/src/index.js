8434e9df660ccc46c55ece75326ce1128a7a84d6
Victor Hugo <victorcobain@hotmail.com>
2021-11-23 13:37:50 -0400

    add

--- react/src/index.js
+++ react/src/index.js
@@ -2,12 +2,20 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./src/style.css"
 
+function primeiroJSX() {
+    return (
+        <div>
+            Victor Hugo - Introdução ao ReactJS
+        </div>
+    )
+}
+
 const App = () => {
     return (
         <div className="App">
-            Hello World
+            {primeiroJSX()}
         </div>
-    )
+    )car
 }
 
 const rootElement = document.getElementById('root')
